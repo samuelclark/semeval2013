e739d1072c76c914c1470aad1f16c7731b1781e7
Sam <=>
2014-01-16 21:18:14 -0800

    clean model_classify

--- classifiers/model_classify.py
+++ classifiers/model_classify.py
@@ -1,17 +1,27 @@
-from classify import Classifier
+from .classify import Classifier
+
+
 class ModelClassifier(Classifier):
-	def __init__(self,**kargs):
-		Classifier.__init__(self,tagged_tweets=kargs["tagged_tweets"],instances=kargs["instances"],model=kargs["model"],keys=kargs["keys"])
-		self.cids = kargs["cids"] # classifier ids for score lookups
-		self.classified_tweets = kargs["classified_tweets"]
-		self.id = "model{0}".format(self.num_items)
 
+    """
+            This was an attempt to build a classifier from combinding different models as features
+            This was not used in production, it was replaced by a voting system
+    """
 
+    def __init__(self, **kargs):
+        Classifier.__init__(
+            self,
+            tagged_tweets=kargs["tagged_tweets"],
+            instances=kargs["instances"],
+            model=kargs["model"],
+            keys=kargs["keys"])
+        self.cids = kargs["cids"]  # classifier ids for score lookups
+        self.classified_tweets = kargs["classified_tweets"]
+        self.id = "model{0}".format(self.num_items)
 
-	def build_feature_vector(self,key):
-		features = {}
-		for cid in self.cids:
-			result = self.classified_tweets[key][cid]
-			features["cid={0}".format(cid)] = result
-		return features
-
+    def build_feature_vector(self, key):
+        features = {}
+        for cid in self.cids:
+            result = self.classified_tweets[key][cid]
+            features["cid={0}".format(cid)] = result
+        return features
