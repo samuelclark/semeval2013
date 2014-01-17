f74b9059d09d51611afa108bc506f1d62ec68ab8
Sam <=>
2014-01-16 21:16:42 -0800

    clean length clsasifier

--- classifiers/length_classify.py
+++ classifiers/length_classify.py
@@ -1,25 +1,28 @@
-from classify import Classifier
+from .classify import Classifier
 
 
 class LengthClassifier(Classifier):
-	def __init__(self, **kargs):
-		Classifier.__init__(self,tagged_tweets=kargs["tagged_tweets"],instances=kargs["instances"],merge=kargs["merge"],model=kargs["model"],keys=kargs["keys"])
-		self.id ="length{0}".format(self.num_items)
-		
 
+    """
+            Simple classifier that builds a feature from the length of the context phrase
+            This was not used in the final model :)
+    """
 
-	def build_feature_vector(self,key):
-		# this extracts the lenth of the target phrase as a metric
-		# ERROR IMPROVEMENTS
-		# can we improve this to weight low tweets more towards pos/neg
-		# lots of errors in this classification coming from this
-		
-		return {"target_length":len(self.instances[key])}
-	# OTHER METRICS STUFF
+    def __init__(self, **kargs):
+        Classifier.__init__(
+            self,
+            tagged_tweets=kargs["tagged_tweets"],
+            instances=kargs["instances"],
+            merge=kargs["merge"],
+            model=kargs["model"],
+            keys=kargs["keys"])
+        self.id = "length{0}".format(self.num_items)
 
+    def build_feature_vector(self, key):
+        # this extracts the lenth of the target phrase as a metric
+        # ERROR IMPROVEMENTS
+        # can we improve this to weight low tweets more towards pos/neg
+        # lots of errors in this classification coming from this
 
-	
-		
-
-
-
+        return {"target_length": len(self.instances[key])}
+    # OTHER METRICS STUFF
