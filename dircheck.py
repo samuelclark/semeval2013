6ba2c1cb45d6b43925ae91d61cefa997792eef51
Sam <=>
2014-01-16 20:48:16 -0800

    cleaned dircheck

--- dircheck.py
+++ dircheck.py
@@ -3,6 +3,9 @@ import os
 
 def checkDir(path=os.getcwd(), main="cresults",
              sub="pickles", selection="all", mode="ngram"):
+    """
+        Helper function to check whether a pickle result exists
+    """
     results_path = "{0}/{1}/{2}/{3}".format(main, sub, selection, mode)
     print results_path
     return os.path.isdir(results_path)
@@ -10,6 +13,9 @@ def checkDir(path=os.getcwd(), main="cresults",
 
 def createDir(path=os.getcwd(), main="cresults",
               sub="pickles", selection="all", mode="ngram"):
+    """
+        Helper function to create a diretory for results
+    """
     results_path = "{0}/{1}/{2}/{3}".format(main, sub, selection, mode)
     try:
         print "creating {0}\n".format(results_path)
